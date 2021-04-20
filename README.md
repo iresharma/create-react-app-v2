7fd35f33c5bfe37a25c228815e9f44e7b4c17068
Iresharma <iresh.sharma8@gmail.com>
2021-04-21 05:10:44 +0530

    version 1, ready

---
+++ README.md
@@ -0,0 +1,35 @@
+# create-react-app-v2 🚀
+## This only wroks for Mac/Linux or an system with UNIX on it (WSL inclusive)
+
+I did not like the kind of app the current version of [create-react-app](https://create-react-app.dev) npx package makes. Once it's done we need to refactor a lot of code and I don't prefer that.
+
+### ME DEV ME AUTOMATE STUFF
+
+with the same thinking I wrote this shell script which helps you setup a react app with router and all included.
+
+#### This is an extension to create react app and not a replacement with uses cerate react app to first create an app and then modify it.
+
+## How to use
+Just give this script an execute permission, and run it (to keep thing simple just run the following command)
+
+`chmod 777 createReactApp.sh`
+
+### for long term use you add this script as an environment variable
+
+## Please contribute
+
+Todo
+- [ ] Support for various component packages like
+  - [ ] [Ant design](https://ant.design/docs/react/introduce)
+  - [ ] [Material UI](https://material-ui.com)
+- [ ] Support for css frameworks like
+  - [ ] [Bulma](https://bulma.io)
+  - [ ] [Bootstrap](https://getbootstrap.com)
+  - [ ] [Tailwind](https://tailwindcss.com)
+- [ ] Setup of few npm packages like
+  - [ ] [Axios](https://www.npmjs.com/package/axios)
+
+### Most important
+- [ ] Support for state-management packages
+
+### PS: I am new to react hence I'll be slowly contributing to this, but I expect the community to help it grow
---
+++ createReactApp.sh
@@ -0,0 +1,324 @@
+# @author: Iresharma
+
+# Color variables for color coding text
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo "${BLUE}Running create-react-app-v2 🚀${NC}"
+npx create-react-app $1
+cd "$1/"
+npm install -S react-useanimations
+
+# Moving into the react app directory
+clear
+echo "${YELLOW}Moving into the react app directory 📂${NC}"
+
+# Deleting F up files
+echo "${YELLOW}deleting unwanted files 🗑${NC}"
+rm -rf src/
+
+# Making new src
+echo "${YELLOW}Making src 💻${NC}"
+mkdir src
+cd src/
+
+# Making required directories
+echo "${YELLOW}Making required directories${NC}"
+for i in "assets" "components" "pages" "css"
+do
+    mkdir $i
+    echo "Made $i"
+done
+
+# Moving assets
+cp ../public/logo192.png ./assets/
+cp ../public/logo512.png ./assets/
+
+echo "${RED}Do you want webVitals and texts ?(y/N)${NC}"
+read WEB_AND_TEST
+
+if [ "$WEB_AND_TEST" = "y" ]; then
+cat > reportWebVitals.js << EOF
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
+
+EOF
+
+cat > setupTests.js << EOF
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+EOF
+
+# Index.js
+echo "${YELLOW}Writing index.js${NC}"
+cat > index.js << EOF
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import App from "./App.jsx";
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+EOF
+
+
+else
+
+# Index.js
+echo "${YELLOW}Writing index.js${NC}"
+cat > index.js << EOF
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import App from "./App.jsx";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+EOF
+fi
+
+# Creating footer component 
+cd components/
+cat > footer.jsx << EOF
+import UseAnimations from "react-useanimations";
+import github from "react-useanimations/lib/github"
+import linkedin from "react-useanimations/lib/linkedin"
+import instagram from "react-useanimations/lib/instagram"
+
+const Footer = () => {
+  const styles = {
+    position: "fixed",
+    bottom: 0,
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "black",
+    height: "3vh",
+    width: "100%",
+    color: "white",
+    paddingLeft: 5,
+    paddingRight: 5,
+  };
+  return (
+    <>
+      <div style={styles}>
+          <span>Made with 🧠 by Iresharma</span>
+          <span style={{display: 'flex'}}>
+            <UseAnimations onClick={() => window.open('https://github.com/iresharma', '_blank')} animation={github} strokeColor="white" /> &nbsp;&nbsp;&nbsp;
+            <UseAnimations onClick={() => window.open('https://www.linkedin.com/in/iresharma/', '_blank')} animation={linkedin} strokeColor="white" /> &nbsp;&nbsp;&nbsp;
+            <UseAnimations onClick={() => window.open('https://www.instagram.com/iresharma.py/', '_blank')} animation={instagram} strokeColor="white" /> &nbsp;&nbsp;&nbsp;
+          </span>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
+EOF
+
+cd ../
+
+cd pages/
+# Creating a page
+cat > home.jsx << EOF
+const Home = () => {
+    return (
+        <div class="body">
+            <h1>
+                Welcome to create-react-App 2.0 by <code><a href="https://github.com/iresharma">@Iresharma</a></code>
+            </h1>
+            <img src="https://cdn.dribbble.com/users/904433/screenshots/5705775/context_dribbble.png" alt="" />
+        </div>
+    );
+}
+
+export default Home;
+EOF
+
+cd ../../
+echo `pwd`
+echo "${RED}Want a react-router-dom ? (y/N)${NC}"
+read ROUTER
+
+# App.jsx
+if [ "$ROUTER" = "y" ]; then
+echo "${YELLOW}Working on router 🗺${NC}"
+npm install react-router-dom
+clear
+cd src/
+cd pages/
+
+# Creating another page
+cat > about.jsx << EOF
+const About = () => {
+    return <h1>This is about page</h1>;
+};
+
+export default About;
+EOF
+
+cd ../
+mkdir router
+cd router/
+echo "${YELLOW}Writing router file${NC}"
+
+
+cat > index.jsx << EOF
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../pages/home";
+import About from "../pages/about";
+
+const RouterView = () => {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          {/* <Route path="*" component={Page404} /> */}
+        </Switch>
+      </Router>
+    </>
+  );
+};
+
+export default RouterView;
+EOF
+
+cd ../
+echo "${YELLOW}Writing App.jsx${NC}"
+cat > App.jsx << EOF
+
+
+import RouterView from "./router";
+import Footer from "./components/footer";
+
+const App = () => {
+  return (
+    <>
+      <RouterView />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
+EOF
+
+
+else
+
+echo "${YELLOW}Writing App.jsx${NC}"
+
+cat > App.jsx << EOF
+import Home from "../pages/home"
+import Footer from "../components/footer"
+
+const App = () => {
+    return (
+        <>
+            <Home />
+            <Footer />
+        </>
+    )
+}
+
+export default App;
+EOF
+
+fi
+
+cd css/
+echo "${YELLOW}Writing CSS file 👩🏻‍🎨${NC}"
+cat > index.css << EOF
+* {
+    margin: 0;
+    padding: 0;
+}
+.body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+}
+a {
+    text-decoration: none;
+    color: #f3bccc;
+}
+h1 {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 3em;
+    font-weight: 700;
+    color: #0DD3AC;
+}
+EOF
+
+cd ../
+echo "${RED}Include Firebase 🔥(y/N)${NC}"
+read FIRE
+
+if [ "$FIRE" = "y" ]; then
+mkdir firebase
+cd firebase/
+cat > firebaseConfig.js << EOF
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const expo = { db, auth }
+
+export default expo;
+EOF
+
+cd ../../
+cat > .env << EOF
+REACT_APP_apiKey=
+REACT_APP_authDomain=
+REACT_APP_projectId=
+REACT_APP_storageBucket=
+REACT_APP_messagingSenderId=
+REACT_APP_appId=
+EOF
+echo ".env" >> .gitignore
+fi
+
+git add .
+git commit -m "Made changes uses create-react-app-v2 by @Iresharma"
+
+clear
+echo "${GREEN}React app created${NC}"
+
+npm start
